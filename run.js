c7b21d71358ed9f4856eb9cd0a3c7db0f36776e6
Jake Garrison <omonoid@uw.edu>
2016-10-28 15:14:19 -0700

    make more elegeant

--- run.js
+++ run.js
@@ -21,38 +21,35 @@ var settings = {
 
 // Scrape google
 // console.log('\nscraping google...');
-scraper.google(settings)
-	.then(scraper.bing(settings))
-	.then(scraper.yahoo(settings))
-	.then(scraper.picsearch(settings))
-	.then(process.exit())
-	.catch(function(err) {
-		console.log('Catch err', err);
-	});
-// 
-// // Scrape bing
-// // console.log('\nscraping bing...');
-// scraper.bing(settings, function(err, res) {
-//   if (err) {
-//     console.log('err in bing', err);
-//   }
-// });
-// 
-// // Scrape yahoo
-// // console.log('\nscraping yahoo...');
-// scraper.yahoo(settings, function(err, res) {
-//   if (err) {
-//     console.log('err in yahoo', err);
-//   }
-// });
-// 
-// // Scrape picsearch
-// // console.log('\nscraping yahoo...');
-// scraper.picsearch(settings, function(err, res) {
-//   if (err) {
-//     console.log('err in picsaerch', err);
-//   }
-// });
+scraper.google(settings, function(err, res) {
+  if (err) {
+    console.log('err in google', err);
+  }
+});
+
+// Scrape bing
+// console.log('\nscraping bing...');
+scraper.bing(settings, function(err, res) {
+  if (err) {
+    console.log('err in bing', err);
+  }
+});
+
+// Scrape yahoo
+// console.log('\nscraping yahoo...');
+scraper.yahoo(settings, function(err, res) {
+  if (err) {
+    console.log('err in yahoo', err);
+  }
+});
+
+// Scrape picsearch
+// console.log('\nscraping yahoo...');
+scraper.picsearch(settings, function(err, res) {
+  if (err) {
+    console.log('err in picsaerch', err);
+  }
+});
 
 // console.log('Finished:', new Date().toUTCString());
 // console.log('Exit');
